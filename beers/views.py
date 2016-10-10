5510467b609b8c182465a30ef839edc9c2c762c3
George T. Gougoudis <george.gougoudis@gmail.com>
2016-10-10 17:51:06 +0300

    Change templates dir

--- beers/views.py
+++ beers/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Beer
 
 
 def index(request):
-    return HttpResponse("Helo world")
+    total = Beer.objects.all().count()
+    return render(request, 'beers/index.html', {'total': total})
+
+
+def beer_detail(request, beer_id):
+    response = 'Beer id: %s' % beer_id
+    return HttpResponse(response)
