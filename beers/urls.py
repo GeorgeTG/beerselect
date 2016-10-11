4f8b3b5499a2f791c36f0e6aa6b978717d3fc66f
George T. Gougoudis <george.gougoudis@gmail.com>
2016-10-12 02:17:46 +0300

    Basic app working

--- beers/urls.py
+++ beers/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
         url(r'^beer/(?P<pk>[0-9]+)/$',
             views.BeerDetailView.as_view(),
             name='beer_detail'),
-        url(r'^random/(?P<style_id>[0-9]+)/$',
+        url(r'^random/$',
             views.BeerRandomView.as_view(),
             name='beer_random'),
         url(r'^brewery/(?P<pk>[0-9]+)/$',
--- beers/views.py
+++ beers/views.py
@@ -1,20 +1,35 @@
 import random
 
 from django.http import Http404
-from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from django.views import generic
 from django.core.urlresolvers import reverse
 
-from .models import Beer, Brewery
+from .models import Beer, Brewery, BeerStyle
 
 
 class IndexView(generic.base.TemplateView):
     template_name = "index.html"
 
+    def post(self, request, *args, **kwargs):
+        beers = Beer.objects.filter(
+                style__id=int(request.POST.get('style_id')))
+        try:
+            beer = random.choice(beers)
+        except IndexError:
+            raise Http404("No beers in this category")
+        return redirect(reverse('beer_detail', kwargs={'pk': beer.id}))
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['beers_count'] = Beer.objects.all().count()
         context['breweries_count'] = Brewery.objects.all().count()
+        nonzero = list()
+        for style in BeerStyle.objects.all():
+            if style.beer_set.count() > 0:
+                nonzero.append(style)
+        context['styles'] = nonzero
+
         return context
 
 
