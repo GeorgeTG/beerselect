a2f6c825efc9cdb556fb20ff409636afec9ddb76
George T. Gougoudis <george.gougoudis@gmail.com>
2016-10-12 04:21:55 +0300

    remove secret from settings

--- beerselect/settings.py
+++ beerselect/settings.py
@@ -12,15 +12,16 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
+from .secret import KEY
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECRET_KEY = KEY
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4#89z5^5ybqo)7*djfqk(ckayk3r(#^=!=)aiw-i-bn&6r0b&^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
